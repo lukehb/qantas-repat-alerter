7e9f445fbdc5d6de9ebcb4e6d1ab32c8fc2f77a7
Luke <1215582+lukehb@users.noreply.github.com>
2021-01-30 16:34:57 +1000

    Refactor to add check the IFTTT endpoint and began dockerising script.

---
+++ build.bat
@@ -0,0 +1,1 @@
+docker build -t qantas-alerter-app .
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 import requests
 import argparse
 import time
+import sys
 from bs4 import BeautifulSoup
 
 URL = 'https://www.qantas.com/gb/en/travel-info/travel-updates/coronavirus/qantas-international-network-changes/flights-from-london.html'
@@ -11,19 +12,25 @@ ALERT_URL = 'https://maker.ifttt.com/trigger/page_updated/with/key/{key}'
 PREV_TEXT = None
 REQUEST_FREQUENCY_SECS = 60
 
-# Get the IFTTT Maker key that is passed in on the command line, or else, fail out.
+# Get the GITHUB key that is passed in on the command line, or else, fail out.
 IFTTT_KEY = None
 parser = argparse.ArgumentParser(description="Checks QANTAS website for updates about repatriation flights from UK to Aus.")
-parser.add_argument("-k","--key", help="The IFTTT key used to send an SMS alert when the QANTAS site changes.", required=True)
-parser.add_argument("-f","--frequency", help="How often to check the site (in seconds).", default=60, type=int)
+parser.add_argument("-k","--key", help="IFTTT webhook key to trigger alerts using my phone.", required=True)
+parser.add_argument("-f","--frequency", help="How often to check the site (in seconds).", default=30, type=int)
 args = parser.parse_args()
 IFTTT_KEY = args.key
 REQUEST_FREQUENCY_SECS = args.frequency
-print("Got IFTTT key: {0}".format(args.key))
-print("Will check the site every {0} seconds".format(args.frequency))
+print("Got IFTTT key: {0}".format(IFTTT_KEY))
 
+# Test the IFTTT endpoint to ensure it is valid.
+print("Testing IFTTT endpoint...")
+iftttTestReq = requests.get("https://maker.ifttt.com/trigger/TEST/with/key/{0}".format(IFTTT_KEY))
+if not iftttTestReq.ok:
+    sys.exit("IFTTT endpoint did not return OK request.")
+print(iftttTestReq.text)
 
 # Start sending requests to QANTAS url.
+print("Will check the site every {0} seconds (CTRL + C to exit).".format(args.frequency))
 while True:
     r = requests.get(URL)
 
@@ -48,6 +55,6 @@ while True:
     PREV_TEXT = pageText
 
     # Only send requests at specified frequency.
-    print(".", end="")
+    print(".", end="", flush=True)
     time.sleep(REQUEST_FREQUENCY_SECS)
     
